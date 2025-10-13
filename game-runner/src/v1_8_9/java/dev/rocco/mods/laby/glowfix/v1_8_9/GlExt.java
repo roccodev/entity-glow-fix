faa26ea0dcd87fb01a1b40b2edbf835c11a710e5
RoccoDev <hey@rocco.dev>
2025-10-13 22:09:57 +0200

    gl: don't outline translucent parts of most skins

--- game-runner/src/v1_8_9/java/dev/rocco/mods/laby/glowfix/v1_8_9/GlExt.java
+++ game-runner/src/v1_8_9/java/dev/rocco/mods/laby/glowfix/v1_8_9/GlExt.java
@@ -1,5 +1,6 @@
 package dev.rocco.mods.laby.glowfix.v1_8_9;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
@@ -18,11 +19,17 @@ public class GlExt {
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, OpenGlHelper.GL_COMBINE);
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_CONSTANT);
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_RGB, GL11.GL_REPLACE);
+
+    // Don't outline over translucent parts
+    GlStateManager.enableBlend();
+    GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 1);
   }
 
   public static void disableOutlineMode() {
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_RGB, GL11.GL_TEXTURE);
     GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_RGB, GL11.GL_MODULATE);
+
+    GlStateManager.disableBlend();
   }
 }
